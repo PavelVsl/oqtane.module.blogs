86057bcd714b44f5783d36af15f37acb21ab45d9
Shaun Walker <shaun.walker@siliqon.com>
2019-05-06 13:49:10 -0400

    initial commit

---
+++ Module.cs
@@ -0,0 +1,16 @@
+﻿using Oqtane.Modules;
+
+namespace Oqtane.Module.Sample
+{
+    public class Module : IModule
+    {
+        public string Name { get { return "Sample Module"; } }
+        public string Description { get { return "Sample Module"; } }
+        public string Version { get { return "0.0.1"; } }
+        public string Owner { get { return ""; } }
+        public string Url { get { return ""; } }
+        public string Contact { get { return ""; } }
+        public string License { get { return ""; } }
+        public string Dependencies { get { return ""; } }
+    }
+}
