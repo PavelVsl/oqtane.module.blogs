5ba8bd22653af5664578bd9aa0e48834cc8103a8
Shaun Walker <shaun.walker@siliqon.com>
2019-08-14 09:41:24 -0400

    Updated module sample to a real world blog scenario

---
+++ Client/Services/IBlogService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Oqtane.Module.Blogs.Models;
+
+namespace Oqtane.Module.Blogs.Services
+{
+    public interface IBlogService 
+    {
+        Task<List<Blog>> GetBlogsAsync(int ModuleId);
+
+        Task<Blog> GetBlogAsync(int BlogId);
+
+        Task AddBlogAsync(Blog Blog);
+
+        Task UpdateBlogAsync(Blog Blog);
+
+        Task DeleteBlogAsync(int BlogId);
+    }
+}
---
+++ Server/Repository/BlogContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Oqtane.Module.Blogs.Models;
+using Oqtane.Repository;
+using Oqtane.Modules;
+using Microsoft.AspNetCore.Http;
+
+namespace Oqtane.Module.Blogs.Repository
+{
+    public class BlogContext : DBContextBase, IService
+    {
+        public virtual DbSet<Blog> Blog { get; set; }
+
+        public BlogContext(ITenantResolver TenantResolver, IHttpContextAccessor accessor) : base(TenantResolver, accessor)
+        {
+            // ContextBase handles multi-tenant database connections
+        }
+    }
+}
